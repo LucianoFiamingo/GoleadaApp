7516cd9bc7df7dda73cf384cd66067e2d5efd1ca
lucho <lucianofiamingo@gmail.com>
2020-11-20 13:06:25 -0300

    JugadoresController.cs y vistas de alta y lista.

---
+++ GoleadaApp/MVC/Controllers/JugadoresController.cs
@@ -0,0 +1,42 @@
+﻿using DAL.Entities.EDMX;
+using Services;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class JugadoresController : Controller
+    {
+        JugadoresService JugadoresService;
+       public JugadoresController()
+        {
+            GoleadaDBEntities contexto = new GoleadaDBEntities();
+            JugadoresService = new JugadoresService(contexto);
+        }
+        public ActionResult ListaJugadores()
+        {
+            List<Jugador> Jugadores = JugadoresService.ObtenerTodos();
+            return View(Jugadores);
+        }
+        public ActionResult AltaJugador()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AltaJugador(Jugador Jugador)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["Creado"] = "FALSE";
+                return View(Jugador);
+            }
+
+            JugadoresService.Alta(Jugador);
+            TempData["Creado"] = Jugador.Nombre.ToString();
+            return RedirectToAction("ListaJugadores");
+
+        }
+
+    }
+}
